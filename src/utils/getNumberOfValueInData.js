510ed4119b192b8f03e33c413de4d6d26571a6b3
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-01-27 18:48:32 +1100

    delete getNumberOfValueInData

--- src/utils/getNumberOfValueInData.js
+++
@@ -1,6 +0,0 @@
-// export function getNumberOfValueInData(value, data) {
-//   const number = data.reduce((total, item) => {
-//     return total + (item.value === stateName ? 1 : 0);
-//   }, 0);
-//   return number;
-// }
